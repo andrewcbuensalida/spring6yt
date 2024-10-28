64b63e90741048e0a869484fd9a11f82dc199dd7
Andrew Buensalida <andrewcbuensalida@gmail.com>
2024-10-28 07:43:47 -0700

    notes

--- Part28-Project using Spring Search Feature/src/main/java/com/telusko/ecomproj/model/Employee.java
+++ Part28-Project using Spring Search Feature/src/main/java/com/telusko/ecomproj/model/Employee.java
@@ -1,6 +1,7 @@
 package com.telusko.ecomproj.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,10 +14,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Employee {
+  // to specify other details, use @Column
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
   private String username;
+  @Column(unique = true) // if you want to give this constraint a better name than a random UK_asdfasdfasdf, specify it in the @Table annotation
   private String email;
 }
